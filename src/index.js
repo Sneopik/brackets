c9ffc6010dd580ee186a32c2a42f620400867b30
Roman Piskun <sneopik.romka@gmail.com>
2019-10-15 17:01:34 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let stack = [];
+
+  if (str.length % 2 != 0) 
+    return false;
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (bracketsConfig[j][0] == '|')
+        bracketsConfig[j][0] == '<'
+      if (bracketsConfig[j][1] == '|')
+        bracketsConfig[j][1] == '>';
+    }
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === bracketsConfig[j][0])
+        stack.push(str[i]);
+        
+      let last = stack[stack.length - 1];
+
+      if (str[i] == bracketsConfig[j][1] && last == bracketsConfig[j][0]) {
+        stack.pop();
+      }
+        
+    }
+  }
+
+  if (stack.length == 0)
+    return true;
+  else 
+    return false;
+
 }
